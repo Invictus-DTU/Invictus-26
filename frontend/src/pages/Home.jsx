2be271413ae060283d10a348fafcb0c2c276988e
Zahanboi <zahansharma123@gmail.com>
2026-01-23 14:55:33 +0530

    aligining faqs and footer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import Landing from '@/components/Landing/Landing'
+import FAQ from "@/components/FAQ/faq";
+import Footer from "@/components/Footer/footer";
+
 export default function Home({setLotusClass, setLotusStyle, setFigureClass, setFigureStyle}) {
   return (
-    <Landing setLotusClass={setLotusClass} setLotusStyle={setLotusStyle} setFigureClass={setFigureClass} setFigureStyle={setFigureStyle}/>
+    <><Landing setLotusClass={setLotusClass} setLotusStyle={setLotusStyle} setFigureClass={setFigureClass} setFigureStyle={setFigureStyle} />
+    <FAQ setLotusClass={setLotusClass} setLotusStyle={setLotusStyle} setFigureClass={setFigureClass} setFigureStyle={setFigureStyle}/>
+    <Footer setLotusClass={setLotusClass} setLotusStyle={setLotusStyle} setFigureClass={setFigureClass} setFigureStyle={setFigureStyle}/>
+    </>
   )
 }
